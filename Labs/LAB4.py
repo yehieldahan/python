90521bbe7957518e56eb58e98bea07aabb9a8aab
Yehiel Dahan <dc02dc03@gmail.com>
2021-02-01 10:38:52 +0200

    LAB4

---
+++ Labs/LAB4.py
@@ -0,0 +1,10 @@
+ditels_list=["yehiel dahan" , 28 , "mail:dc02dc03@gmail.com" , "0546662345"]
+print("full name: " + ditels_list[0] + "\nage: " + str((ditels_list)[1]) + "\nmail: " + ditels_list[2]  + "\nphone: " + ditels_list[3])
+ip_list=["1.1.1.1" , "2.2.2.2"]
+print(ip_list)
+ip_list.append("3.3.3.3")
+ip_list.append("4.4.4.4")
+ip_list.append("5.5.5.5")
+print(ip_list)
+ip_list.pop(2)
+print("\nIP List length is: " + str(len(ip_list)) + "\nAnd and IP List: " + str(ip_list))
